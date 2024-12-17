9f2d4fc7858ffaf7035f25cb79d01cbac371303f
Adrian-Caiza <adrian.caiza@epn.edu.ec>
2024-12-17 08:19:20 -0500

    Pagina ASOESFOT completa

---
+++ asoesfot-Main/acciones.js
@@ -0,0 +1,13 @@
+alert("Bienbenido a la ESFOT")
+    var val1=2;
+    var val2=3;
+    var suma;
+
+suma= val1+val2
+alert("la suma es: "+ suma);
+
+let a=22;
+let b=33;
+let suma1;
+suma1=a+b
+alert("La suma con let es: "+suma1);
